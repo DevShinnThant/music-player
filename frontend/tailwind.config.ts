d193f5ce7ec2bcd5f11b332997881197d129b720
shinnthant2004 <shinthantequi@gmail.com>
2024-01-03 15:29:47 +0630

    chore: package installation and setup

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,12 +1,12 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
@@ -18,6 +18,12 @@ const config = {
       },
     },
     extend: {
+      colors: {
+        main: "#ECF2FD",
+        primary: "#FFFFFF",
+        active: "#FF7E3A",
+        secondary: "#2E3271",
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
@@ -35,6 +41,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
