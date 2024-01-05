7c582c9f22758bfa2925765ce7b9bce31cb68a9e
shinnthant2004 <shinthantequi@gmail.com>
2024-01-05 15:08:37 +0630

    feat: play bar ui

--- frontend/components/layout/ParentLayout.tsx
+++ frontend/components/layout/ParentLayout.tsx
@@ -2,6 +2,8 @@
 
 import React from "react";
 import Sidebar from "./Sidebar";
+import TopLayout from "./TopLayout";
+import Playbar from "./PlayBar";
 
 export default function ParentLayout({
   children,
@@ -11,7 +13,11 @@ export default function ParentLayout({
   return (
     <div className="max-width h-screen flex items-center">
       <Sidebar />
-      <div className="bg-main w-full h-full">{children}</div>
+      <div className="relative bg-main w-full h-full flex flex-col">
+        <TopLayout />
+        <div className="px-7 flex-1 overflow-y-scroll">{children}</div>
+        <Playbar />
+      </div>
     </div>
   );
 }
---
+++ frontend/components/layout/PlayBar.tsx
@@ -0,0 +1,69 @@
+import Image from "next/image";
+import {
+  Heart,
+  Next,
+  PlayCircle,
+  Previous,
+  RepeateOne,
+  Shuffle,
+  VolumeHigh,
+} from "iconsax-react";
+import { Slider } from "../ui/slider";
+
+export default function Playbar() {
+  return (
+    <div className="absolute overflow-hidden bottom-0 w-full h-[108px] bg-gray-500 backdrop-filter backdrop-blur-sm bg-opacity-10 ">
+      <div className="w-full h-full grid grid-cols-12">
+        <div className="col-span-3">
+          <div className="flex h-full gap-5 items-center justify-start px-5">
+            <Image
+              width={64}
+              height={64}
+              className="rounded-lg"
+              alt="song"
+              src="https://github.com/shadcn.png"
+            />
+            <div className="flex flex-col gap-1 items-start">
+              <div className="text-sm text-gray-300 font-medium">
+                Higher Power
+              </div>
+              <div className="text-xs text-textPrimary">Coldplay</div>
+            </div>
+            <Heart className="cursor-pointer" size={20} color="white" />
+          </div>
+        </div>
+        <div className="col-span-6">
+          <div className="flex h-full flex-col gap-5 items-center justify-center">
+            <div className="flex items-center gap-6">
+              <Shuffle className="cursor-pointer" size={20} color="gray" />
+              <Previous className="cursor-pointer" size={20} color="#919693" />
+              <PlayCircle
+                className="cursor-pointer"
+                color="white"
+                size={36}
+                variant="Bulk"
+              />
+              <Next className="cursor-pointer" size={20} color="#919693" />
+              <RepeateOne
+                className="cursor-pointer"
+                size={20}
+                color="#919693"
+              />
+            </div>
+            <div className="w-full flex items-center justify-center gap-3">
+              <div className="text-xs text-textSecondary">1:12</div>
+              <Slider className="text-active w-4/6" />
+              <div className="text-xs text-textSecondary">3:28</div>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-3 p-3 flex items-center justify-center">
+          <div className="w-full flex items-center justify-end gap-3">
+            <VolumeHigh size={18} color="white" />
+            <Slider className=" w-3/6" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
