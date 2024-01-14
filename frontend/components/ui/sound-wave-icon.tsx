d9417e621207a2b2be1f86255231c869f81f6741
shinnthant2004 <shinthantequi@gmail.com>
2024-01-14 13:49:46 +0630

    feat: sound wave icon animation

---
+++ frontend/components/ui/sound-wave-icon.tsx
@@ -0,0 +1,11 @@
+export default function SoundWaveIcon() {
+  return (
+    <div className="h-[12px] flex items-center ">
+      <div className="sound-wave-bar" />
+      <div className="sound-wave-bar" />
+      <div className="sound-wave-bar" />
+      <div className="sound-wave-bar" />
+      <div className="sound-wave-bar" />
+    </div>
+  );
+}
