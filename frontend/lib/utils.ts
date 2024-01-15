227e7e625afa1e273d8f8923570eede87006fc3b
shinnthant2004 <shinthantequi@gmail.com>
2024-01-15 14:26:20 +0630

    feat: util fnc for duration milisec to sec

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatDuration(durationSeconds: number) {
+  const minutes = Math.floor(durationSeconds / 60);
+  const seconds = Math.floor(durationSeconds % 60);
+  const formattedSeconds = seconds.toString().padStart(2, "0");
+  return `${minutes}:${formattedSeconds}`;
+}
