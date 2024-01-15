81168c3568e17af82fc068e46300ddf0e50670e0
shinnthant2004 <shinthantequi@gmail.com>
2024-01-15 16:53:39 +0630

    refactor: playbar display logic

--- frontend/components/layout/ParentLayout.tsx
+++ frontend/components/layout/ParentLayout.tsx
@@ -1,19 +1,24 @@
+"use client";
+
 import Sidebar from "./Sidebar";
 import TopLayout from "./TopLayout";
 import Playbar from "./PlayBar";
+import { useMusicStore } from "@/lib/store/client/music";
 
 export default function ParentLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { currentMusic } = useMusicStore();
+
   return (
     <div className="max-width overflow-auto h-screen flex items-center">
       <Sidebar />
       <div className="relative bg-main w-full h-full flex flex-col">
         <TopLayout />
         <div className="px-7 h-[calc(100vh_-_100px)]">{children}</div>
-        <Playbar />
+        {currentMusic && <Playbar />}
       </div>
     </div>
   );
--- frontend/components/layout/PlayBar.tsx
+++ frontend/components/layout/PlayBar.tsx
@@ -1,49 +1,129 @@
+"use client";
+
 import Image from "next/image";
 import {
   Heart,
   Next,
+  PauseCircle,
   PlayCircle,
   Previous,
   RepeateOne,
   Shuffle,
   VolumeHigh,
 } from "iconsax-react";
-import { Slider } from "../ui/slider";
+
+import { useMusicStore } from "@/lib/store/client/music";
+import { useEffect, useRef, useState } from "react";
+
+import { Input } from "../ui/input";
+import { formatDuration } from "@/lib/utils";
 
 export default function Playbar() {
+  const { currentMusic } = useMusicStore();
+
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+
+  const intervalRef = useRef<any>({});
+  const isReady = useRef<boolean>(false);
+
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
+  const [currentTime, setCurrentTime] = useState<number>(0);
+
+  // Destructure for conciseness
+  const duration = audioRef.current?.duration || 0;
+
+  useEffect(() => {
+    if (!audioRef.current) return;
+
+    if (isPlaying) {
+      audioRef.current.play();
+      startTimer();
+    } else {
+      clearInterval(intervalRef.current);
+      audioRef.current.pause();
+    }
+  }, [isPlaying]);
+
+  useEffect(() => {
+    // Pause and clean up on unmount
+    return () => {
+      audioRef.current?.pause();
+      clearInterval(intervalRef.current);
+    };
+  }, []);
+
+  const startTimer = () => {
+    if (intervalRef.current && audioRef.current) {
+      // Clear any timers already running
+      clearInterval(intervalRef.current);
+
+      intervalRef.current = setInterval(() => {
+        if (audioRef.current?.ended) {
+          // toNextTrack();
+        } else {
+          // setTrackProgress(audioRef.current.currentTime);
+          setCurrentTime(audioRef.current?.currentTime || 0);
+        }
+      }, 1000);
+    }
+  };
+
+  useEffect(() => {
+    if (!audioRef.current) return;
+    audioRef.current.pause();
+
+    audioRef.current = new Audio(currentMusic?.song);
+    setCurrentTime(audioRef.current.currentTime);
+
+    if (isReady.current) {
+      audioRef.current.play();
+      setIsPlaying(true);
+      startTimer();
+    } else {
+      isReady.current = true;
+    }
+  }, [currentMusic]);
+
   return (
-    <div className="absolute overflow-hidden bottom-0 w-full h-[108px] bg-gray-500 backdrop-filter backdrop-blur-sm bg-opacity-10 ">
+    <div className="fixed bottom-0 w-[calc(100vw_-_260px)] h-[108px] bg-gray-500 backdrop-filter backdrop-blur-sm bg-opacity-10 ">
       <div className="w-full h-full grid grid-cols-12">
         <div className="col-span-3">
           <div className="flex h-full gap-5 items-center justify-start px-5">
             <Image
-              priority
               width={64}
               height={64}
               className="rounded-lg"
               alt="song"
-              src="https://github.com/shadcn.png"
+              src={currentMusic?.image || ""}
             />
             <div className="flex flex-col gap-1 items-start">
               <div className="text-sm text-gray-300 font-medium">
-                Higher Power
+                {currentMusic?.name}
               </div>
-              <div className="text-xs text-textPrimary">Coldplay</div>
+              <div className="text-xs text-textPrimary">
+                {currentMusic?.artist}
+              </div>
             </div>
             <Heart className="cursor-pointer" size={20} color="white" />
           </div>
         </div>
         <div className="col-span-6">
+          <audio hidden ref={audioRef} />
           <div className="flex h-full flex-col gap-5 items-center justify-center">
             <div className="flex items-center gap-6">
               <Shuffle className="cursor-pointer" size={20} color="gray" />
               <Previous className="cursor-pointer" size={20} color="#919693" />
-              <PlayCircle
+              <div
+                onClick={() => setIsPlaying((prev) => !prev)}
                 className="cursor-pointer"
-                color="white"
-                size={36}
-                variant="Bulk"
-              />
+              >
+                {isPlaying ? (
+                  <PauseCircle color="white" size={36} variant="Bulk" />
+                ) : (
+                  <PlayCircle color="white" size={36} variant="Bulk" />
+                )}
+              </div>
               <Next className="cursor-pointer" size={20} color="#919693" />
               <RepeateOne
                 className="cursor-pointer"
@@ -52,16 +132,36 @@ export default function Playbar() {
               />
             </div>
             <div className="w-full flex items-center justify-center gap-3">
-              <div className="text-xs text-textSecondary">1:12</div>
-              <Slider className="text-active w-4/6" />
-              <div className="text-xs text-textSecondary">3:28</div>
+              <div className="text-xs text-textSecondary">
+                {formatDuration(currentTime)}
+              </div>
+
+              <Input
+                min={0}
+                max={duration}
+                value={currentTime}
+                onChange={(e) => {
+                  console.log("hit");
+                }}
+                type="range"
+                className=" in-range:border-r in-range:border-r-emerald-400"
+              />
+              <div className="text-xs text-textSecondary">
+                {formatDuration(duration)}
+              </div>
             </div>
           </div>
         </div>
         <div className="col-span-3 p-3 flex items-center justify-center">
           <div className="w-full flex items-center justify-end gap-3">
             <VolumeHigh size={18} color="white" />
-            <Slider className="w-3/6" />
+            {/* <Slider
+              onClick={(e) => {
+                console.log(e);
+              }}
+              className="w-3/6"
+            /> */}
+            <Input type="range" />
           </div>
         </div>
       </div>
