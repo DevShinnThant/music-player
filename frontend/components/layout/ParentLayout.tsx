5476ab6c1a23d42d244759dec36130a6ffe7df5c
shinnthant2004 <shinthantequi@gmail.com>
2024-01-16 16:23:28 +0630

    fix: playbar logic

--- frontend/components/layout/ParentLayout.tsx
+++ frontend/components/layout/ParentLayout.tsx
@@ -10,7 +10,7 @@ export default function ParentLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const { currentMusic } = useMusicStore();
+  const { currentMusicIndex } = useMusicStore();
 
   return (
     <div className="max-width overflow-auto h-screen flex items-center">
@@ -18,7 +18,7 @@ export default function ParentLayout({
       <div className="relative bg-main w-full h-full flex flex-col">
         <TopLayout />
         <div className="px-7 h-[calc(100vh_-_100px)]">{children}</div>
-        {currentMusic && <Playbar />}
+        {currentMusicIndex !== null ? <Playbar /> : null}
       </div>
     </div>
   );
