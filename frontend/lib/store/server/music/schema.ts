98a71cfc5f88ac42dd9e9703ea13f06707a9b7b5
shinnthant2004 <shinthantequi@gmail.com>
2024-01-16 16:24:11 +0630

    fix: music api response type error

--- frontend/lib/store/server/music/schema.ts
+++ frontend/lib/store/server/music/schema.ts
@@ -40,34 +40,36 @@ const musicDataSchema = z.object({
         caption: z.string().nullish(),
         width: z.number().nullish(),
         height: z.number().nullish(),
-        formats: z.object({
-          thumbnail: z
-            .object({
-              name: z.string(),
-              hash: z.string(),
-              ext: z.string(),
-              mime: z.string(),
-              path: z.string().nullish(),
-              width: z.number(),
-              height: z.number(),
-              size: z.number(),
-              url: z.string(),
-            })
-            .nullish(),
-          small: z
-            .object({
-              name: z.string(),
-              hash: z.string(),
-              ext: z.string(),
-              mime: z.string(),
-              path: z.string().nullish(),
-              width: z.number(),
-              height: z.number(),
-              size: z.number(),
-              url: z.string(),
-            })
-            .nullish(),
-        }),
+        formats: z
+          .object({
+            thumbnail: z
+              .object({
+                name: z.string(),
+                hash: z.string(),
+                ext: z.string(),
+                mime: z.string(),
+                path: z.string().nullish(),
+                width: z.number(),
+                height: z.number(),
+                size: z.number(),
+                url: z.string(),
+              })
+              .nullish(),
+            small: z
+              .object({
+                name: z.string(),
+                hash: z.string(),
+                ext: z.string(),
+                mime: z.string(),
+                path: z.string().nullish(),
+                width: z.number(),
+                height: z.number(),
+                size: z.number(),
+                url: z.string(),
+              })
+              .nullish(),
+          })
+          .nullable(),
         hash: z.string(),
         ext: z.string(),
         mime: z.string(),
