d43d935084457a4c2a6d571faccd1a9e2fa493b3
shinnthant2004 <shinthantequi@gmail.com>
2024-01-17 11:07:33 +0630

    refactor: music fetch with limit

--- frontend/lib/store/server/music/get.ts
+++ frontend/lib/store/server/music/get.ts
@@ -6,7 +6,7 @@ const API_URL = process.env.NEXT_PUBLIC_DATABASE_URL;
 
 export const getMusics = async () => {
   const response = await axios.get(
-    `${API_URL}/api/musics?pagination[page]=1&pagination[pageSize]=6&populate=*`
+    `${API_URL}/api/musics?pagination[page]=1&pagination[pageSize]=7&populate=*`
   );
 
   const modifedData = musicSelector(musicApiResSchema.parse(response.data));
