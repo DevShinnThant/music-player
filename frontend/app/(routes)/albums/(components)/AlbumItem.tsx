edb34e2fb91b2ed0efe41e9432f18f98c85edbce
shinnthant2004 <shinthantequi@gmail.com>
2024-01-18 15:59:17 +0630

    feat: album page ui

--- frontend/app/(routes)/albums/(components)/AlbumItem.tsx
+++ frontend/app/(routes)/albums/(components)/AlbumItem.tsx
@@ -1,15 +1,30 @@
+"use client";
+
+import { useMusicStore } from "@/lib/store/client/music";
 import { SelectAlbum } from "@/lib/store/server/album/types";
-import { Play } from "iconsax-react";
+import { cn } from "@/lib/utils";
+import { Pause, Play } from "iconsax-react";
 import Image from "next/image";
 
 interface Props {
   item: SelectAlbum;
+  active: boolean;
 }
 
-export default function AlbumItem({ item }: Props) {
+export default function AlbumItem({ item, active }: Props) {
+  const {
+    insertMusics,
+    setCurrentMusic,
+    setCurrentAlbum,
+    isPlaying,
+    togglePlay,
+    currentAlbumId,
+  } = useMusicStore();
+
   return (
     <div
       key={item.id}
+      onClick={() => {}}
       className="col-span-2 relative group flex flex-col items-start gap-4 p-4 rounded-lg h-56 bg-gray-600 bg-opacity-10 hover:bg-opacity-30 transition cursor-pointer"
     >
       <Image
@@ -24,8 +39,26 @@ export default function AlbumItem({ item }: Props) {
         <div className="text-xs text-textSecondary">{item.artist}</div>
       </div>
 
-      <div className=" z-10 absolute flex items-center justify-center translate-x-[100px] translate-y-32 opacity-0 group-hover:opacity-90 transition-transform group-hover:translate-y-28 w-10 h-10 rounded-full bg-blue-600">
-        <Play variant="Bold" size={20} color="white" />
+      <div
+        onClick={(e) => {
+          if (currentAlbumId !== item.id) {
+            insertMusics(item.songs);
+            setCurrentAlbum(item.id);
+            setCurrentMusic(item.songs[0].id);
+          } else {
+            togglePlay();
+          }
+        }}
+        className={cn(
+          `z-10 absolute flex items-center justify-center translate-x-[100px] duration-300 translate-y-32 opacity-0 group-hover:opacity-90 transition-transform group-hover:translate-y-28 w-10 h-10 rounded-full bg-blue-600`,
+          active && "opacity-100"
+        )}
+      >
+        {isPlaying ? (
+          <Pause variant="Bold" size={16} color="white" />
+        ) : (
+          <Play variant="Bold" size={20} color="white" />
+        )}
       </div>
     </div>
   );
---
+++ frontend/app/(routes)/albums/(components)/AlbumSection.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { SelectAlbum } from "@/lib/store/server/album/types";
+import AlbumItem from "./AlbumItem";
+import { useMusicStore } from "@/lib/store/client/music";
+
+interface Props {
+  data: SelectAlbum[];
+}
+
+export default function AlbumSection({ data }: Props) {
+  const { currentAlbumId } = useMusicStore();
+
+  // console.log(isPlaying, "rerender & is Playing");
+
+  return (
+    <div className="grid grid-cols-12 gap-6">
+      {data.map((album) => (
+        <AlbumItem
+          active={currentAlbumId === album.id}
+          key={album.id}
+          item={album}
+        />
+      ))}
+    </div>
+  );
+}
